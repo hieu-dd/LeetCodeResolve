4fb5ff91ed55516efdb90521f9ffa51af50ab24f
hieu-dd <hieu.dd@teko.vn>
2022-02-04 12:46:43 +0700

    + remove duplicate // khai xuan 2022

--- src/main/kotlin/RemoveDuplicate.kt
+++ src/main/kotlin/RemoveDuplicate.kt
@@ -1,7 +1,23 @@
 fun removeDuplicates(nums: IntArray): Int {
-    return nums.distinct().size
+    var i = 0
+    var j = 0
+    val size = nums.size
+    var count = 0
+    while (j < size) {
+        val check = nums[j]
+        j++
+        while (j < size && nums[j] == check) {
+            count++
+            j++
+        }
+        if (j >= size) {
+            break
+        }
+        nums[++i] = nums[j]
+    }
+    return nums.size - count
 }
 
-fun main(){
-    print(removeDuplicates(intArrayOf(1,1,2)))
+fun main() {
+    print(removeDuplicates(intArrayOf(1, 1)))
 }
